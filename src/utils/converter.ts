fffed21ca7d0b55e50b2476f21a543e945ab17b3
pqppq <yutatsu0119@gmail.com>
2022-04-17 19:55:10 +0900

    fix

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,13 +1,13 @@
 import { Result, Ok, Err } from "./result.ts";
 import { Range } from "../list/range.ts";
 
-export function toRanges(
+export function listToRanges(
   flag: string,
   list: string,
   complement: boolean
 ): Result<Range[], string> {
   if (!list) {
-    return new Err<Range[], string>(
+    return new Err(
       `teip: Expected argument for flag '${flag}' but reached end of arguments.`
     );
   }
@@ -16,8 +16,9 @@ export function toRanges(
     if (res.isErr()) {
       return res;
     }
-    return new Ok<Range[], string>(Range.complement(res.value));
+    return new Ok(Range.complement(res.value));
   } else {
     return Range.fromList(list);
   }
 }
+
